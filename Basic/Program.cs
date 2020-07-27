980103d39702a3c9639d75569ea5669698370a77
WELCOME <WELCOME@DESKTOP-VNDV0B2>
2020-07-27 12:30:46 +0530

    https://github.com/poojitha-pancheti/console.git

---
+++ Basic/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Basic
+{
+	class Program
+	{
+		static void Main(String[] args)
+		{
+			int a = 42;
+			int b = 119;
+			int c = a + b;
+			Console.WriteLine(c);
+			Console.ReadKey();
+		}
+	}
+}
